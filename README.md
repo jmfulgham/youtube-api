864199c0e05e2ee202c5aadb857b95cd85b37dd9
Mone't Fulgham <monetfulgham@gmail.com>
2017-12-02 15:01:55 -0500

    updated all files

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # youtube-api
-YouTube API Thinkful assignment
+YouTube API 
 
-This is my first attempt to use an API. I was so excited to complete this assignment. Even where it got a little frustrating, I was happy to be learning this skill. I got some help from a Q&A session, but overall, I was glad to learn I was really close and on the right track. I will be exploring how to utilize and build API's. This is something I've always wanted to learn about and perfect.
+This is my first attempt to use an API. I was so excited to complete this assignment. Initially I created this without using Bootstrap to practice using API's. 
+
+My wireframe is <a href="https://wireframe.cc/IkXpJa">here</a>
--- index.js
+++ index.js
@@ -1,49 +1,58 @@
-const youtubeSearchURL ='https://www.googleapis.com/youtube/v3/search';
+var youtubeSearchURL = 'https://www.googleapis.com/youtube/v3/search';
+var nextPageToken = null;
 
-function getApiData (searchTerm, callback){
-	const query={
-		part: 'snippet',
-		key: 'AIzaSyCR1A18yeOJ8ngGXDMB3F2S3hfS2JB2Q1E',
-		q: `${searchTerm}`,
-		per_page: 6
-		
-	}
-	$.getJSON(youtubeSearchURL, query, callback);
-		// .fail(showErr);
-	console.log(query);
-	
+function getApiData (){
+  console.log("here is nextPageToken", nextPageToken);
+  var searchCriteria = $('input[type=text]').val();
+  _getApiData(searchCriteria, showResults, nextPageToken)
+    .then((response) => nextPageToken = response.nextPageToken)
 };
 
-// function showErr(err){
-// 	var errorMsg= `<p>I'm sorry we couldn't find that!</p>`;
-// 	$('.theresults').append(errorMsg);
-
-// }
-
-function showResults(results){
-	 $.each(results.items, function (videosArrayKey, videosArrayValue) {
-	$('.theresults').append(`<div class="singleResults"><a class="result-name" href="https://www.youtube.com/watch?v=${videosArrayValue.id.videoId}
-		 target="_blank"}>${videosArrayValue.snippet.title}<Br><img src="${videosArrayValue.snippet.thumbnails.default.url}" 
-		 href="https://www.youtube.com/watch?v=${videosArrayValue.id.videoId} target="_blank"}></a></div><Br>`); 
-	});
-	console.log(results);
-
-
+var clickSubmit = event => {
+  event.preventDefault();
+  formReset();
+  getApiData();
 };
 
-function formReset (){
-	$( '.theresults' ).empty();
+var goToNextPage = event => {
+  event.preventDefault();
+  formReset();
+  // get the next page token
+  // call getAPIDATA
+  getApiData();
 };
 
-function clickSubmit () {
-	$('button').on('click', event => {
-		event.preventDefault();
-		formReset();
-				var searchCriteria= $('input[type=text]').val();
-		console.log('here is the search criteria: ' + searchCriteria);
-		getApiData(searchCriteria, showResults);
-		
-	});
+var formReset = () => {
+  $('.theresults').empty();
 };
 
-$(clickSubmit);
+// Register listeners
+$('.search').on('click', clickSubmit);
+$('.next').on('click', goToNextPage);
+
+//
+function _getApiData(searchTerm, callback, pageToken) {
+  var query = {
+    part: 'snippet',
+    key: 'AIzaSyCR1A18yeOJ8ngGXDMB3F2S3hfS2JB2Q1E',
+    q: searchTerm,
+    per_page: 5,
+    pageToken: pageToken
+  }
+  return $.getJSON(youtubeSearchURL, query, callback);
+};
+
+function showResults(results) {
+  console.log(results);
+  $.each(results.items, function(videosArrayKey, videosArrayValue) {
+    $('.invisible').removeClass('invisible');
+    $('footer').removeClass('invisible');
+    $('.theresults').append(`<div class="singleResult"><a class="result-name text-danger" 
+      href="https://www.youtube.com/watch?v=${videosArrayValue.id.videoId}
+     target="_blank"}>${videosArrayValue.snippet.title}
+     <img class="w-100" src="${videosArrayValue.snippet.thumbnails.high.url}" 
+     href="https://www.youtube.com/watch?v=${videosArrayValue.id.videoId} 
+     target="_blank"}></a><div class="description">
+     ${videosArrayValue.snippet.description}</div><div>`);
+  });
+};
